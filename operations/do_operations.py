ed9b02abdfde0dfffdb927492a26b93508fd9b36
JoseABermudez57 <jose.apep570@gmail.com>
2024-01-14 07:03:01 -0600

    [CREATE] Algorithm with iterations

---
+++ operations/do_operations.py
@@ -0,0 +1,34 @@
+from fractions import Fraction
+import pandas as pd
+from operations import genetic_algorithm as ga
+
+
+def start_simulation(data):
+    a = int(data["a"])
+    b = int(data["b"])
+    pob_min = int(data["pob_min"])
+    prob_mut_individual = float(data["prob_mut_individual"])
+    prob_mut_gene = float(data["prob_mut_gene"])
+    res = float(Fraction(data["res"]))
+    pob_max = int(data["pob_max"])
+    iterations = int(data["iterations"])
+    max_or_min = data["max_or_min"]
+
+    qt_bits, delta_x = ga.gen_base_values(a, b, res)
+    ga.gen_individuals(pob_min, qt_bits, a, delta_x)
+
+    df = pd.DataFrame(ga.data)
+    df.index = df.index + 1
+
+    print(df)
+
+    for _ in range(iterations):
+        couples = ga.generate_couples(ga.data.get('Individuo'), len(ga.data))
+        children = ga.crossover_pairs(couples)
+        mutate_children = ga.mutate_pob(children, prob_mut_individual, prob_mut_gene)
+        ga.data.get('Individuo').extend(mutate_children)
+        print(ga.data.get("Individuo"))
+
+    print(df)
+
+    print("_________________________")
