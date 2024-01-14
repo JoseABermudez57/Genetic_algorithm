52d66de76dcc5d004cea9b524dd91be275b7faf2
JoseABermudez57 <jose.apep570@gmail.com>
2024-01-13 18:29:12 -0600

    [UPDATE] genetic_algorithm to an object-oriented class

--- operations/genetic_algorithm.py
+++ operations/genetic_algorithm.py
@@ -1,23 +1,37 @@
 import math
+import random
 
 
-def gen_base_values(a, b, bits_length, delta_x_tch):
-    ranges = b - a
-    jumps_tch = ranges / delta_x_tch
-    points_tch = jumps_tch + 1
-    qt_bits_tch = math.ceil(math.log(points_tch, 2))
-    if 2 ** (qt_bits_tch - 1) < points_tch <= 2 ** qt_bits_tch:
-        print("correcto", qt_bits_tch)
-    else:
-        print("incorrecto", qt_bits_tch)
+class GeneticAlgorithm:
+    def __init__(self, a, b, bits_length, delta_x_tch, min_population, jumps):
+        self.a = a
+        self.b = b
+        self.bits_length = bits_length
+        self.delta_x_tch = delta_x_tch
+        self.min_population = min_population
+        self.jumps = jumps
+        self.data = {
+            'Individuo': [],
+            'i': [],
+            'x': [],
+            'f(x)': []
+        }
 
-    print("_______________________")
+    def gen_base_values(self):
+        ranges = self.b - self.a
+        jumps_tch = ranges / self.delta_x_tch
+        points_tch = jumps_tch + 1
+        qt_bits_tch = math.ceil(math.log(points_tch, 2))
+        if 2 ** (qt_bits_tch - 1) < points_tch <= 2 ** qt_bits_tch:
+            print("correcto", qt_bits_tch)
+        else:
+            print("incorrecto", qt_bits_tch)
 
-    delta_x = ranges / (pow(2, bits_length) - 1)
-    jumps = ranges / delta_x
-    points = jumps + 1
-    qt_bits = math.ceil(math.log(points, 2))
-    if 2 ** (qt_bits - 1) < points <= 2 ** qt_bits:
-        print("correcto", qt_bits)
-    else:
-        print("incorrecto", qt_bits)
+        delta_x = ranges / (pow(2, self.bits_length) - 1)
+        jumps = ranges / delta_x
+        points = jumps + 1
+        qt_bits = math.ceil(math.log(points, 2))
+        if 2 ** (qt_bits - 1) < points <= 2 ** qt_bits:
+            print("correcto", qt_bits)
+        else:
+            print("incorrecto", qt_bits)
