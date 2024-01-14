5964d92cdb3278677d469dfd406ce9382fc32723
JoseABermudez57 <jose.apep570@gmail.com>
2024-01-13 19:05:53 -0600

    [ADD] generate individuals method

--- operations/genetic_algorithm.py
+++ operations/genetic_algorithm.py
@@ -1,4 +1,12 @@
 import math
+import random
+
+data = {
+    'Individuo': [],
+    'i': [],
+    'x': [],
+    'f(x)': []
+}
 
 
 def gen_base_values(a, b, bits_length, delta_x_tch):
@@ -23,3 +31,15 @@ def gen_base_values(a, b, bits_length, delta_x_tch):
         print("correcto", qt_bits)
     else:
         print("incorrecto", qt_bits)
+
+
+def gen_individuals(min_population, jumps, qt_bits, a, delta_x):
+    random_nums = [random.randint(0, jumps) for _ in range(min_population)]
+    binary_nums = [format(int(num), f'0{qt_bits}b') for num in random_nums]
+    x_values = [round(a + i * delta_x, 4) for i in random_nums]
+    fx_values = [round((i**3 - 2 * (i**2) * math.cos(math.radians(i)) + 3), 4) for i in x_values]
+
+    data['Individuo'].extend(binary_nums)
+    data['i'].extend(random_nums)
+    data['x'].extend(x_values)
+    data['f(x)'].extend(fx_values)
