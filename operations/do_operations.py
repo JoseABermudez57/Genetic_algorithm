d7a373215af7d258017c69eeae1a829dc8eea421
JoseABermudez57 <jose.apep570@gmail.com>
2024-01-14 09:38:41 -0600

    [UPDATE] operations

--- operations/do_operations.py
+++ operations/do_operations.py
@@ -1,6 +1,7 @@
 from fractions import Fraction
 import pandas as pd
 from operations import genetic_algorithm as ga
+import matplotlib.pyplot as plt
 
 
 def start_simulation(data):
@@ -10,24 +11,22 @@ def start_simulation(data):
     prob_mut_individual = float(data["prob_mut_individual"])
     prob_mut_gene = float(data["prob_mut_gene"])
     res = float(Fraction(data["res"]))
-    pob_max = int(data["pob_max"])
     iterations = int(data["iterations"])
+    pob_max = int(data["pob_max"])
     max_or_min = data["max_or_min"]
 
     qt_bits, delta_x = ga.gen_base_values(a, b, res)
     ga.gen_individuals(pob_min, qt_bits, a, delta_x)
 
-    df = pd.DataFrame(ga.data)
-    df.index = df.index + 1
-
-    print(df)
-
     for _ in range(iterations):
         couples = ga.generate_couples(ga.data.get('Individuo'), len(ga.data))
         children = ga.crossover_pairs(couples)
         ga.mutate_pob(children, prob_mut_individual, prob_mut_gene, delta_x, a)
-        print(ga.data.get("Individuo"))
+        print(ga.data.get('Individuo'))
 
-    print(pd.DataFrame(ga.data))
+    df = pd.DataFrame(ga.data)
+    df.to_csv('estadisticos.csv')
 
-    print("_________________________")
+    pruning = ga.pruning(max_or_min, pob_max)
+
+    print(pruning)
